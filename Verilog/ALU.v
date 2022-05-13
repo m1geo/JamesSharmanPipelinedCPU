be3054b4e25b523c3c177eaea9f94c79e9336d2d
George Smart <github@george-smart.co.uk>
2022-05-13 18:28:15 +0100

    Placeholder added for remaining ALU stuff (add, flags, zero cmp)

---
+++ Verilog/ALU.v
@@ -0,0 +1,21 @@
+/*
+
+Name:				ALU?
+Schematic Source:   ??
+Schematic Rev:		??
+Designer:			James Sharman (weirdboyjim)
+
+FPGA/Verilog: 		George Smart (@m1geo) http://www.george-smart.co.uk
+Project Source:		https://github.com/m1geo/JamesSharmanPipelinedCPU
+
+Module notes:
+	See video part 34, diagram at 32 seconds: https://www.youtube.com/watch?v=pMV_0qT0uY0?t=32
+	The entire ALU is shown.
+	Verilog is missing for "ADD", "FLAGS" and "ZERO CMP".
+*/
+
+module ALU ();
+
+	// main code goes here!
+
+endmodule
