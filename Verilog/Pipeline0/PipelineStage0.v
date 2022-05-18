2374e0dcb75ce04f15a5f892986b0584bc6547d6
George Smart <george@Georges-Air.local>
2022-05-19 00:03:16 +0100

    Untested Pipeline 0

--- Verilog/Pipeline0/PipelineStage0.v
+++ Verilog/Pipeline0/PipelineStage0.v
@@ -1,6 +1,6 @@
 /*
 
-Name:				Pipeline Stage 0
+Name:				Pipeline Stage 0 (Fetch)
 Schematic Source:   https://easyeda.com/weirdboyjim/Pipeline-Stage-0
 Schematic Rev:		1.0 (2019-01-13)
 Designer:			James Sharman (weirdboyjim)
@@ -9,8 +9,8 @@ FPGA/Verilog: 		George Smart (@m1geo) http://www.george-smart.co.uk
 Project Source:		https://github.com/m1geo/JamesSharmanPipelinedCPU
 
 Module notes:
-	Just an empty module with ports.
-	
+	Video: https://www.youtube.com/watch?v=GkUddJNc24c
+	Just a simple latch to hold the pipe data, and a mux to chose current or latched data
 */
 
 module PipelineStage0 (
@@ -28,6 +28,16 @@ module PipelineStage0 (
 	input			Flags_5_PCRA_Flip
 );
 
-	// main code goes here!
+	reg [7:0] PipeLatch;
+	
+	always @ (posedge ClockIn) begin
+		PipeLatch <= MEMDATA;
+	end
+	
+	// MUXes to select state output
+	assign PipeOut[i] = FetchSurpress ? (BusRequest ? PipeLatch : 8'b0') : (BusRequest ? 8'b0 : MEMDATA);
+	
+	assign Pipe0Out_0_IncPCRA0 = Flags_5_PCRA_Flip;
+	assign Pipe0Out_1_IncPCRA1 = BusRequest;
 
 endmodule
