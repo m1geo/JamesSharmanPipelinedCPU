c0041671a6457f52f701d47a558e76c47e51e720
George Smart <george@Georges-MacBook-Air.local>
2022-12-26 00:45:22 +0000

    RHS Logic Unit

--- Verilog/ALU/README.md
+++ Verilog/ALU/README.md
@@ -5,5 +5,5 @@ The table below shows the status of the different files in this folder:
 | adder.v | Adder | ✅ | ❌ |
 | diode_matrix.v | ALU-OP Diode Matrix | ✅ | ✅ |
 | flags.v | FLAGS | ❌ | ❌ |
-| logic.v | ALU RHS Logic Unit | ❌ | ❌ |
+| logic.v | ALU RHS Logic Unit | ✅ | ❌ |
 | shift.v | ALU LHS Shift Unit | ✅ | ❌ |
--- Verilog/ALU/logic.v
+++ Verilog/ALU/logic.v
@@ -7,10 +7,21 @@
 // GitHub  : https://github.com/m1geo/JamesSharmanPipelinedCPU
 // CPU Dsn : James Sharman; Video Series => https://youtu.be/3iHag4k4yEg
 //
-// Desc.   : Perform ALU logic operations on LHS or RHS
+// Desc.   : Perform ALU logic operations on LHS and/or RHS
+//	       : James Sharman design based on Julian Ilett's video
+//	       :   https://www.youtube.com/watch?v=15M63Zqkthk
+//
+//         : Functions provided:
+//         :   (LogicSelect=4'b1100) RHS straight through
+//         :   (LogicSelect=4'b0011) inverted RHS (for subtraction) 
+//         :   (LogicSelect=4'b0000) Zero operation
+//         :   (LogicSelect=4'b1111) 255 output
+//         :   (LogicSelect=4'b1000) LHS AND RHS
+//         :   (LogicSelect=4'b1110) LHS OR RHS
+//         :   (LogicSelect=4'b0110) LHS XOR RHS
 // -----------------------------------------------------------------------------
 
-module logic
+module logic_rhs
 (
 	input        clk,
 	input  [3:0] LogicSelect,
@@ -20,6 +31,23 @@ module logic
     output [7:0] RHSOut
 );
 
-    // code here
+	// mux outputs 
+	wire [7:0] mux_out;
+	
+	// Loop to unroll the MUXes
+	genvar i;
+	generate
+		for (i = 0; i < 8 ; i = i + 1) begin // for each bit in 8-bit bus
+			assign mux_out[i] = RHSIn[i] ? (LHSIn[i] ? LogicSelect[3] : LogicSelect[2]) : (LHSIn[i] ? LogicSelect[1] : LogicSelect[0]);
+		end
+	endgenerate 
 
-endmodule //end:logic
+	// registers on the RHS PCB (on control breadboard)
+	reg [7:0] reg_da = 0;
+	always @ (posedge clk) begin
+		reg_da <= mux_out;
+	end
+	
+	assign RHSOut = reg_da; // data out
+
+endmodule //end:logic_rhs
